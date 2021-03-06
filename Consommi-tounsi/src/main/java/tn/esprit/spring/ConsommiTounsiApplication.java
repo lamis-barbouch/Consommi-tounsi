919c73c1474d6b8ef52d3975e4bd25818793a95d
dell <dell@192.168.84.1>
2021-03-06 11:25:27 +0100

    First Commit.

---
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/ConsommiTounsiApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsommiTounsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsommiTounsiApplication.class, args);
+	}
+
+}
