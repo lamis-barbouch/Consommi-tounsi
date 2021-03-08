8e109d790dd311f4b7574da5dc4eeea18c26324b
NOUR <NOUR@DESKTOP-8NRGJGJ>
2021-03-08 22:10:09 +0100

    .

--- Consommi-tounsi/src/main/java/tn/esprit/spring/entity/CodePromo.java
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/CodePromo.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity 
 public class CodePromo implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
