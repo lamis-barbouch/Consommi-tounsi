cd71ee9665743d093899ddaadd7bbad18c342a4b
dell <dell@dell-PC>
2021-03-08 19:37:55 +0100

    verion 0

---
+++ Consommi-tounsi/src/main/java/tn/esprit/entity/Comment.java
@@ -0,0 +1,48 @@
+package tn.esprit.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Comment implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id_comment;
+	private int id_user;
+	private String text_comment;
+	
+	
+	public int getId_comment() {
+		return id_comment;
+	}
+	public void setId_comment(int id_comment) {
+		this.id_comment = id_comment;
+	}
+	public int getId_user() {
+		return id_user;
+	}
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+	public String getText_comment() {
+		return text_comment;
+	}
+	public void setText_comment(String text_comment) {
+		this.text_comment = text_comment;
+	}
+	public Comment(int id_comment, int id_user, String text_comment) {
+		super();
+		this.id_comment = id_comment;
+		this.id_user = id_user;
+		this.text_comment = text_comment;
+	}
+	public Comment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
---
+++ Consommi-tounsi/src/main/java/tn/esprit/entity/Donation.java
@@ -0,0 +1,14 @@
+package tn.esprit.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Donation  implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id_donation;
+}
