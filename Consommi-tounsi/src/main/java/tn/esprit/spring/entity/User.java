721d74300016fd90885dbfde0dca33c6da23699f
Takwa <Takwa@LAPTOP-GVQGAFKB>
2021-03-08 21:02:40 +0100

    user

---
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/User.java
@@ -0,0 +1,91 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class User implements Serializable {
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id_user;
+    private String first_name ;
+    private String last_name ;
+    private int phone_number;
+    private String email ;
+    private String login;
+    private String password;
+    private String gender ;
+    @Temporal (TemporalType.DATE)
+	private Date date ;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId_user() {
+		return id_user;
+	}
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public int getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(int phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+    
+   
+   
+     
+	
+
+}
