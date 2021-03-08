80028afff431931256b5dc845cc97d23cb4e30b5
Takwa <Takwa@LAPTOP-GVQGAFKB>
2021-03-08 21:36:58 +0100

    .

--- Consommi-tounsi/src/main/java/tn/esprit/spring/entity/User.java
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/User.java
@@ -7,10 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public class User implements Serializable {
     @Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
