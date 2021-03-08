989e846cb972de49b1670c67935bf72f9475b46c
Takwa <Takwa@LAPTOP-GVQGAFKB>
2021-03-08 21:47:17 +0100

    ..

--- Consommi-tounsi/src/main/java/tn/esprit/spring/entity/Admin.java
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/Admin.java
@@ -10,5 +10,27 @@ public class Admin extends User {
    private int rib;
    private String dicForbiddenWords;
    private float salary;
+public int getRib() {
+	return rib;
+}
+public void setRib(int rib) {
+	this.rib = rib;
+}
+public String getDicForbiddenWords() {
+	return dicForbiddenWords;
+}
+public void setDicForbiddenWords(String dicForbiddenWords) {
+	this.dicForbiddenWords = dicForbiddenWords;
+}
+public float getSalary() {
+	return salary;
+}
+public void setSalary(float salary) {
+	this.salary = salary;
+}
+public Admin() {
+	super();
+	// TODO Auto-generated constructor stub
+}
 
 }
---
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/DeliveryMan.java
@@ -0,0 +1,52 @@
+package tn.esprit.spring.entity;
+
+public class DeliveryMan extends User {
+	
+	private int rating_deliveryMan;
+	private int cin;
+	private String address;
+	private float salary;
+	private float bonus;
+	private String city;
+	public int getRating_deliveryMan() {
+		return rating_deliveryMan;
+	}
+	public void setRating_deliveryMan(int rating_deliveryMan) {
+		this.rating_deliveryMan = rating_deliveryMan;
+	}
+	public int getCin() {
+		return cin;
+	}
+	public void setCin(int cin) {
+		this.cin = cin;
+	}
+	public DeliveryMan() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public float getBonus() {
+		return bonus;
+	}
+	public void setBonus(float bonus) {
+		this.bonus = bonus;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
