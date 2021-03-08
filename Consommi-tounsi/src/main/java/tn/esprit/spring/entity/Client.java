76b8e2a31a9d574b4d1fcfa3fba3ee1653603659
Takwa <Takwa@LAPTOP-GVQGAFKB>
2021-03-08 21:46:49 +0100

    .

--- Consommi-tounsi/src/main/java/tn/esprit/spring/entity/Client.java
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/Client.java
@@ -6,5 +6,21 @@ import javax.persistence.Entity;
 public class Client extends User {
 	private String address;
 	private String city;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Client() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 }
