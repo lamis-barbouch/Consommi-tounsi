a50d714db371fa6b3a14aec4ede2f1d4d3a0c380
dell <dell@dell-PC>
2021-03-08 20:31:04 +0100

    version 1 rectification

---
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/Fund.java
@@ -0,0 +1,39 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Fund implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id_fund;
+	//float
+	private float amount;
+	public int getId_fund() {
+		return id_fund;
+	}
+	public void setId_fund(int id_fund) {
+		this.id_fund = id_fund;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public Fund() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Fund(int id_fund, float amount) {
+		super();
+		this.id_fund = id_fund;
+		this.amount = amount;
+	}
+	
+	
+}
---
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/SubjectForum.java
@@ -0,0 +1,56 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SubjectForum implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id_subject;
+	private String subject;
+	private String title;
+	private int number_LikesSubject;
+	public int getId_subject() {
+		return id_subject;
+	}
+	public void setId_subject(int id_subject) {
+		this.id_subject = id_subject;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getNumber_LikesSubject() {
+		return number_LikesSubject;
+	}
+	public void setNumber_LikesSubject(int number_LikesSubject) {
+		this.number_LikesSubject = number_LikesSubject;
+	}
+	public SubjectForum(int id_subject, String subject, String title, int number_LikesSubject) {
+		super();
+		this.id_subject = id_subject;
+		this.subject = subject;
+		this.title = title;
+		this.number_LikesSubject = number_LikesSubject;
+	}
+	public SubjectForum() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
