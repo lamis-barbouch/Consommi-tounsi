a083a3ea5f74a3d11261862a5258e1225bc1cd5c
Takwa <Takwa@LAPTOP-GVQGAFKB>
2021-03-17 17:02:46 +0100

    ..

--- Consommi-tounsi/src/main/java/tn/esprit/spring/entity/Admin.java
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/entity/Admin.java
@@ -4,43 +4,18 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
+@Entity
 
-<<<<<<< HEAD
 public class Admin extends User
 {
 	@OneToMany(mappedBy="admin")
 	List<Ad> ads;
-=======
-@Entity
-public class Admin extends User {
-	/**
-	 * 
-	 */
-	
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-    private int id_user;
-	
->>>>>>> branch 'main' of https://github.com/lamis-barbouch/Consommi-tounsi.git
 	//@OneToMany(mappedBy="admin")
-<<<<<<< HEAD
 	//List<Event> event;
 	@OneToMany(mappedBy="admin")
 	List<Accounting> accounting;
 	List<DeliveryMan> deliverymens;
-=======
-	//private List<Ad> ads;
-	
-	@OneToMany(mappedBy="admin")
-	private List<Event> event;
-	//@OneToMany(mappedBy="admin")
-	//List<Accounting> accounting;
->>>>>>> branch 'main' of https://github.com/lamis-barbouch/Consommi-tounsi.git
    private int rib;
    private String dicForbiddenWords;
    private float salary;
