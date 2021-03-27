a3bed1e98620c4b7fd102c3b23de8a89e0a4cdd8
Takwa <Takwa@LAPTOP-GVQGAFKB>
2021-03-27 13:55:48 +0100

    11

--- Consommi-tounsi/src/main/java/tn/esprit/spring/services/IUserServices.java
+++ Consommi-tounsi/src/main/java/tn/esprit/spring/services/IUserServices.java
@@ -9,35 +9,15 @@ import tn.esprit.spring.entity.User;
 
 public interface IUserServices {
 
-public List<Client> retrieveAllClients () ;
-public void deleteClient(int id);
-public Client retrieveClient(int id);
-public	User updateClient(Client c);
-public Client addClient(Client c);
-
-
-
-
-public void deleteAdmin(int id);
-public Admin retrieveAdmin(int id);
-public User updateAdmin(Admin a);
-public Admin addAdmin(Admin a);
-
-
-
-
 public User addUser(User u);
 public User updateUser(User u);
 public void deleteUser(int id);
 public User retrieveUser(int id);
 public List<User> retrieveAllUsers();
+public User getUserByUsernameOrEmail(String username);
 
 
 
-public DeliveryMan addDeliveryMan(DeliveryMan d);
-public DeliveryMan updateDeliveryMan(DeliveryMan d);
-public void deletDeliveryMan(int id);
-public DeliveryMan retrieveDeliveryMan(int id);
-public List<DeliveryMan> retrieveAllDeliveryMans();
+
 
 }
